e967f2c177117c7e334cdc865bee1f0ee0d0309c
grace-kathure <gracegikundak@gmail.com>
2022-11-22 02:25:08 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b){
+    return a + b;}
+    console.log(add(1, 80))
+
+
+function subtract(a, b){
+    return a - b;}
+console.log(subtract(60, 40))
+
+function multiply(a, b){
+    return a * b;}
+console.log(multiply(2, 3.4))
+
+function divide(a, b){
+    return a / b;}
+console.log(divide(5, 2.5))
+
+
+function increment(n){
+    return n+1
+}
+
+function decrement (n){
+    return n-1
+}
+function makeInt(n){
+    return parseInt(n, 10)
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
